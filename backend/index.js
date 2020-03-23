f4d11b41e1fac2b2b5113fc611e95028bf1fc696
Bruno Barbosa <barbosa.developer@gmail.com>
2020-03-23 17:19:41 -0300

    Aula 1

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/',(req,resp)=>{
+    return resp.json({
+        evento: 'Semana OminiStack 11.0',
+        aluno: 'Bruno Barbosa'
+    })
+})
+
+app.listen(3333);
